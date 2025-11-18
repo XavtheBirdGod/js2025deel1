cf7ea3c7e7cf5837949857850be13b94d3421af4
Xanthe Vanneste Deloose <xanthe.vannestedeloose@gmail.com>
2025-11-18 14:06:01 +0100

    oefening 1

--- oef1/src/js/main.js
+++ oef1/src/js/main.js
@@ -4,3 +4,51 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 //EIGEN JS
+let oefening = []
+
+
+function maakLijst(){
+    const lijst = document.getElementById("ex1_list")
+
+    lijst.innerHTML = oefening.map((sport) => `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <p>${sport.sport}</p>
+      <p>${sport.tijd} minuten</p>
+      </li>
+    `)
+        .join("");
+    console.log(oefening)
+}
+
+function voegToe(){
+    const sport = document.getElementById("ex1_ex").value
+    const tijd = Number(document.getElementById("ex1_min").value)
+    const status = document.getElementById("ex1_feedback")
+    const count = document.getElementById("ex1_total")
+
+    if(!sport || !tijd){
+        status.className = "alert alert-danger"
+        status.innerText = "Gelieve beide velden in te vullen"
+    }
+
+    const length = oefening.length + 1
+
+    status.className = "alert alert-success"
+    status.innerText = `goed zo, ze hebt al ${length} oefening(en) ingevuld`
+
+    const result = ({sport, tijd})
+
+    let totaal = tijd
+
+    count.innerText = totaal
+
+    oefening.push(result);
+
+    maakLijst()
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    document.getElementById("ex1_btn")?.addEventListener("click", voegToe)
+
+})
